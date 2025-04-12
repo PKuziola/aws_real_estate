3a0dc9f5eb43e24a296c221e097f66d75e8d16b2
Piotr Kuzioła <p.kuziola686@gmail.com>
2025-04-12 21:43:52 +0200

    sadyba_spot spider fix

--- image/source/real_estate_prices/settings.py
+++ image/source/real_estate_prices/settings.py
@@ -99,3 +99,4 @@ FEED_EXPORT_ENCODING = "utf-8"
 #}
 
 #TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
