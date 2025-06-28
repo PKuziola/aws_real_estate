d5627cf7c2fe6796fbd129dc235899f67b1bc94e
Piotr Kuzioła <p.kuziola686@gmail.com>
2025-06-28 13:43:21 +0200

    setting.py gbq fix

--- image/source/real_estate_prices/settings.py
+++ image/source/real_estate_prices/settings.py
@@ -62,9 +62,9 @@ SPIDER_MIDDLEWARES = {
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "real_estate_prices.pipelines.SaveToGBQPipeline": 300,
-#}
+ITEM_PIPELINES = {
+    "real_estate_prices.pipelines.SaveToGBQPipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
