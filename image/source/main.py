4fd4ec8fdb25745a1d49b882f924d94bc5a5d535
Piotr Kuzioła <p.kuziola686@gmail.com>
2026-04-18 00:28:16 +0200

    fix main.py

--- image/source/main.py
+++ image/source/main.py
@@ -1,17 +1,26 @@
 import os
 import sys
+from multiprocessing import Process
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
+
+
+def _run_spiders():
+    settings = get_project_settings()
+    process = CrawlerProcess(settings)
+    for spider_name in process.spider_loader.list():
+        process.crawl(spider_name)
+    process.start()
+
 
 def real_estate_scraper(event, _context):
     sys.path.append('/var/task')
     os.environ['SCRAPY_SETTINGS_MODULE'] = 'source.real_estate_prices.settings'
 
-    settings = get_project_settings()
-    process = CrawlerProcess(settings)
+    p = Process(target=_run_spiders)
+    p.start()
+    p.join()
 
-    for spider_name in process.spider_loader.list():
-        process.crawl(spider_name)
-        print('pies')
 
-    process.start()
+if __name__ == "__main__":
+    real_estate_scraper(None, None)
